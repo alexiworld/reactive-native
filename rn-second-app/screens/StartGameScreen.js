9145bcb04184ed36e5a1ef0ded31efb228235974
Alexi Jordanov <alexi.j@samsung.com>
2020-04-04 15:13:53 -0700

    Add alert for invalid values being entered by the user.

--- rn-second-app/screens/StartGameScreen.js
+++ rn-second-app/screens/StartGameScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TextInput, Button, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Button, TouchableWithoutFeedback, Keyboard, Alert } from 'react-native';
 
 import Card from '../components/Card';
 import Input from '../components/Input';
@@ -20,7 +20,8 @@ const StartGameScreen = props => {
 
     const confirmInputHandler = () => {
         const chosenNumer = parseInt(enteredValue);
-        if (chosenNumer === NaN || chosenNumer <= 0 || chosenNumer > 99) {
+        if (isNaN(chosenNumer) || chosenNumer <= 0 || chosenNumer > 99) {
+            Alert.alert('Invalid number', 'Number has to be a number between 1 and 99', [{text: 'OK', style: 'destructive', onPress: resetInputHandler}]);
             return;
         }
         setConfirmed(true);
